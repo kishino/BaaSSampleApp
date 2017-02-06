67bcb2422e12743418fcd2f2dc0f376997b357ac
kishino <kishino@pscsrv.co.jp>
2017-02-06 12:35:17 +0900

    日付変換処理を追加

--- www/js/controllers/NewsDetailController.js
+++ www/js/controllers/NewsDetailController.js
@@ -16,6 +16,8 @@
     function onReady() {
       // 一覧から渡したお知らせ情報を取得して、ViewModelに設定
       vm.newsDetail = $scope.navi.topPage.data;
+      // 日時データは文字列（yyyy-MM-dd HH:mm:ssZ）のため、Date型に変換する
+      vm.newsDetail.release_end_date = new Date(vm.newsDetail.release_end_date);
     }
   }
 
