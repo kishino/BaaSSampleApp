f9b597d795ddb8e6e6057d2cb8a9da08e8afb772
kishino <kishino@pscsrv.co.jp>
2017-02-06 12:41:48 +0900

    コメント追記

--- www/js/controllers/NewsController.js
+++ www/js/controllers/NewsController.js
@@ -24,12 +24,14 @@
       // BaaS@rakuzaから、上限20件で公開しているお知らせ情報を取得する
       var limit = 20;
       RKZClient.getReleasedNewsList(limit, [], [], function (data) {
+        // 成功時の処理
         // BaaS@rakuzaのコールバックは$scope.$applyを呼ばないので、$timeoutで反映を行う
         $timeout(function () {
           vm.news = data;
           vm.isLoading = false;
         });
       }, function () {
+        // 失敗時の処理
         alert('エラーが発生しました。');
         $timeout(function () {
           vm.isLoading = false;
